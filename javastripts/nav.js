3ccf39002b27df40630951ffa601d22339c2532a
toby-gamez <henematobik213@gmail.com>
2024-12-23 21:54:43 +0100

    24w51e

--- javastripts/nav.js
+++ javastripts/nav.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <a href="about.html">O mně</a>
                 <a href="nazor.html">Vaše zpětná vazba</a>
                 <a href="changelog.html">Deník změn</a>
-                <span style="font-family: 'Outfit', sans-serif;" class="navtext">24w51d</span>
+                <span style="font-family: 'Outfit', sans-serif;" class="navtext">24w51e</span>
 
                 <button class="scroll-to-top" onclick="scrollToTop()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
