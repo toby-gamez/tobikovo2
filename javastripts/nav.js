9db239c5d707fa47d489b2e4cb96d5bf38daad3e
toby-gamez <henematobik213@gmail.com>
2025-02-10 20:41:18 +0100

    25w07a

--- javastripts/nav.js
+++ javastripts/nav.js
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                 </a>
             </li>
-            <div class="version-info">25w05a</div>
+            <div class="version-info">25w07a</div>
             
            <li class="nav-item" style="padding-top: 3rem">
                 <a class="nav-link" href="#">
@@ -247,7 +247,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 </li>
             </ul>
         </div>
-        <div class="version-info">25w05a</div>
+        <div class="version-info">25w07a</div>
     </div>
     `;
 
